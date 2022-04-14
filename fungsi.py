ed08884158c8a0e75b88a6d1b3ab7ad48de48191
dorokdok <90540407+dorokdok@users.noreply.github.com>
2022-04-14 20:28:16 +0700

    AAA

--- fungsi.py
+++ fungsi.py
@@ -6,7 +6,7 @@ listFloat = List[float]
 Fuzifikasi = List[listFloat]
 
 
-def FuzifikasiServis (inputan: int) -> listFloat :
+def FuzifikasiServis (inputan: float) -> listFloat :
     bagus = float
     tengah = float
     jelek = float
@@ -35,4 +35,114 @@ def FuzifikasiServis (inputan: int) -> listFloat :
         jelek = (inputan - 10)/(40-10)
     return [bagus,tengah,jelek]
 
+def FuzifikasiHarga (inputan: float) -> listFloat :
+    mahal = float
+    tengah = float
+    murah = float
+    #mahal
+    if inputan > 8 :
+        mahal = 1
+    elif inputan > 6 :
+        mahal = (inputan-6)/(10-6)
+    else :
+        mahal = 0
+    #Menengah
+    if inputan > 8 or inputan <= 4:
+        tengah = 0
+    elif inputan > 4 and inputan < 5:
+        tengah = 1
+    elif inputan >= 5 and inputan <= 8 :
+        tengah = (8 - inputan)/(8-5)
+    elif inputan > 4 and inputan <= 4 :
+        tengah = (inputan - 4)/(4-4)
+    #murah   
+    if inputan > 4:
+        murah = 0
+    elif inputan > 0 and inputan <= 2:
+        murah = 1
+    elif inputan > 2 and inputan <= 4:
+        murah = (inputan - 2)/(4-2)
+    return [mahal,tengah,murah]
 
+
+def inteference(servis: Fuzifikasi, harga: Fuzifikasi) -> Fuzifikasi:
+    out = Fuzifikasi
+    out = []
+    pasti = listFloat
+    mungkin = listFloat
+    tidak = listFloat
+    
+    for i in range(len(servis)):
+        dmp = Fuzifikasi
+        dmp = []
+        pasti = []
+        mungkin = []
+        tidak = []
+        for j in range(len(servis[i])):
+            if servis[i][j] >= 0.5:
+                if j :
+                    if harga[i][0] >= 0.5:
+                        if servis[i][j] < harga[i][0]:
+                            mungkin.append(servis[i][j])
+                        else:
+                            mungkin.append(harga[i][0])
+                    if harga[i][1] >= 0.5:
+                        if servis[i][j] < harga[i][1]:
+                            pasti.append(servis[i][j])
+                        else:
+                            pasti.append(harga[i][1])
+                    if harga[i][2] >= 0.5:
+                        if servis[i][j] < harga[i][2]:
+                            pasti.append(servis[i][j])
+                        else:
+                            pasti.append(harga[i][2])
+                if j == 1:
+                    if harga[i][0] >= 0.5:
+                        if servis[i][j] < harga[i][0]:
+                            tidak.append(servis[i][j])
+                        else:
+                            tidak.append(harga[i][0])
+                    if harga[i][1] >= 0.5:
+                        if servis[i][j] < harga[i][1]:
+                            mungkin.append(servis[i][j])
+                        else:
+                            mungkin.append(harga[i][1])
+                    if harga[i][2] >= 0.5:
+                        if servis[i][j] < harga[i][2]:
+                            mungkin.append(servis[i][j])
+                        else:
+                            mungkin.append(harga[i][2])
+                if j == 2:
+                    if harga[i][0] >= 0.5:
+                        if servis[i][j] < harga[i][0]:
+                            tidak.append(servis[i][j])
+                        else:
+                            tidak.append(harga[i][0])
+                    if harga[i][1] >= 0.5:
+                        if servis[i][j] < harga[i][1]:
+                            tidak.append(servis[i][j])
+                        else:
+                            tidak.append(harga[i][1])
+                    if harga[i][2] >= 0.5:
+                        if servis[i][j] < harga[i][2]:
+                            mungkin.append(servis[i][j])
+                        else:
+                            mungkin.append(harga[i][2])
+        pasti.sort()
+        mungkin.sort()
+        tidak.sort()
+        if len(pasti) > 0:
+            dmp.append(pasti[0])
+        else:
+            dmp.append(0)
+        if len(mungkin) > 0:
+            dmp.append(mungkin[0])
+        else:
+            dmp.append(0)
+        if len(tidak) > 0:
+            dmp.append(tidak[0])
+        else:
+            dmp.append(0)
+        out.append(dmp)
+
+    return out
--- main.py
+++ main.py
@@ -5,9 +5,13 @@ dataHarga = f.listFloat
 
 fuzifikasiSrvc = f.Fuzifikasi
 fuzifikasiSrvc = []
+fuzifikasiPrc = f.Fuzifikasi
+fuzifikasiPrc = []
+
+rules=f.Fuzifikasi
 
 data = f.pd.read_excel(r'bengkel.xlsx')
-print(data)
+print(data, "\n")
 
 dataServis = data['servis'].tolist()
 dataHarga = data['harga'].tolist()
@@ -15,5 +19,14 @@ dataHarga = data['harga'].tolist()
 for i in range(len(dataServis)):
     fuzifikasiSrvc.append(f.FuzifikasiServis(dataServis[i]))
 
-print(fuzifikasiSrvc)
+print(fuzifikasiSrvc, "\n")
+
+for i in range(len(dataHarga)):
+    fuzifikasiPrc.append(f.FuzifikasiHarga(dataHarga[i]))
+
+print(fuzifikasiPrc,"\n")
+
+rules = f.inteference(fuzifikasiSrvc,fuzifikasiPrc)
+
+print(rules, "\nAAAA")
 
