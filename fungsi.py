e9c29cd93fcd9b9fc3796971b4df1d932020b6a6
dorokdok <90540407+dorokdok@users.noreply.github.com>
2022-04-20 18:38:51 +0700

    A

--- fungsi.py
+++ fungsi.py
@@ -21,28 +21,28 @@ def FuzifikasiServis (inputan: float) -> listFloat :
     else :
         bagus = 0
     #Menengah
-    #Jika servis > 80 atau < 40 maka tengah = 0
-    if inputan > 80 or inputan <= 40:
+    #Jika servis > 85 atau < 40 maka tengah = 0
+    if inputan > 85 or inputan < 30:
         tengah = 0
-    #Jika 45 < servis < 55 maka tengah = 1
-    elif inputan > 45 and inputan < 55:
+    #Jika 50 < servis < 60 maka tengah = 1
+    elif inputan >= 50 and inputan <= 60:
         tengah = 1
-    #Jika 55 <= servis <= 80 maka tengah = rumus
-    elif inputan >= 55 and inputan <= 80 :
-        tengah = (80 - inputan)/(80-55)
-    #Jika 40 < servis <= 45 maka tengah = rumus
-    elif inputan > 40 and inputan <= 45 :
-        tengah = (inputan - 40)/(45-40)
+    #Jika 60 <= servis <= 85 maka tengah = rumus
+    elif inputan > 60 and inputan <= 85 :
+        tengah = (85 - inputan)/(85-60)
+    #Jika 30 < servis <= 50 maka tengah = rumus
+    elif inputan >= 30 and inputan < 50 :
+        tengah = (inputan - 30)/(50-30)
     # jelek    
-    #Jika servis > 40 maka jelek = 0
-    if inputan > 40:
+    #Jika servis > 50 maka jelek = 0
+    if inputan >= 50:
         jelek = 0
-    #Jika servis <= 10 make jelek = 1
-    elif inputan > 0 and inputan <= 10:
+    #Jika servis <= 30 make jelek = 1
+    elif inputan > 0 and inputan < 30:
         jelek = 1
-    #Jika 10 < servis <= 45 maka jelek = rumus
-    elif inputan > 10 and inputan <= 45:
-        jelek = (inputan - 10)/(55-10)
+    #Jika 30 < servis <= 50 maka jelek = rumus
+    elif inputan >= 30 and inputan < 50:
+        jelek = (50 - inputan)/(50-30)
     return [bagus,tengah,jelek]
 
 #Function Fuzifikasi Harga
@@ -61,28 +61,28 @@ def FuzifikasiHarga (inputan: float) -> listFloat :
     else :
         mahal = 0
     #Menengah
-    #Jika harga > 8 atau harga <= 4 maka tengah = 0
+    #Jika harga > 8 atau harga <= 3 maka tengah = 0
     if inputan > 8 or inputan <= 3:
         tengah = 0
-    #Jika 4 < harga < 5 maka tengah = 1
-    elif inputan > 4 and inputan < 5:
+    #Jika 5 < harga < 6 maka tengah = 1
+    elif inputan >= 5 and inputan < 6:
         tengah = 1
-    #Jika 5 < harga <= 7 maka tengah = rumus
-    elif inputan >= 5 and inputan <= 8 :
-        tengah = (8 - inputan)/(8-5)
-    #Jika 3 < harga <= 4 maka tengah = rumus
-    elif inputan > 3 and inputan <= 4 :
-        tengah = (inputan - 3)/(4-3)
+    #Jika 6 < harga <= 8 maka tengah = rumus
+    elif inputan >= 6 and inputan <= 8 :
+        tengah = (8 - inputan)/(8-6)
+    #Jika 3 < harga <= 5 maka tengah = rumus
+    elif inputan > 3 and inputan <= 5 :
+        tengah = (inputan - 3)/(5-3)
     #murah
-    #Jika Harga > 4 maka murah = 0   
-    if inputan > 4:
+    #Jika Harga > 5 maka murah = 0   
+    if inputan > 5:
         murah = 0
-    #Jika harga <= 2 maka murah = 1
-    elif inputan > 0 and inputan <= 2:
+    #Jika harga <= 3 maka murah = 1
+    elif inputan > 0 and inputan <= 3:
         murah = 1
-    #jika 2 < harga <= 4 maka murah = rumus
-    elif inputan > 2 and inputan <= 4:
-        murah = (inputan - 2)/(4-2)
+    #jika 3 < harga <= 5 maka murah = rumus
+    elif inputan > 3 and inputan <= 5:
+        murah = (5 - inputan)/(5-3)
     return [mahal,tengah,murah]
 
 #Inference dengan aturan
